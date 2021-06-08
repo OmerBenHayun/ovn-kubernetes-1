d85c9b229f1769f0b699427333a2dbf8a0fab047
omer <omerben@campus.technion.ac.il>
2021-06-08 16:07:57 +0300

    adding tcpdump flag control

--- dist/images/ovnkube.sh
+++ dist/images/ovnkube.sh
@@ -220,7 +220,7 @@ ovsdb_etcd_schemas_dir=${OVSDB_ETCD_SCHEMAS_DIR:-/root/ovsdb-etcd/schemas}
 ovsdb_etcd_prefix=${OVSDB_ETCD_PREFIX:-"ovsdb"}
 ovsdb_etcd_nb_log_level=${OVSDB_ETCD_NB_LOG_LEVEL:-"5"}
 ovsdb_etcd_sb_log_level=${OVSDB_ETCD_SB_LOG_LEVEL:-"5"}
-ovsdb_etcd_log_to_tcpdump=${OVSDB_ETCD_LOG_TO_TCPDUMP:-"true"}
+ovndb_etcd_tcpdump=${OVNDB_ETCD_TCPDUMP:-"false"}
 
 # Determine the ovn rundir.
 if [[ -f /usr/bin/ovn-appctl ]]; then
@@ -1213,7 +1213,7 @@ ovs-metrics() {
 }
 
 etcd () {
-  if [[ ${ovsdb_etcd_log_to_tcpdump} == "true" ]]; then
+  if [[ ${ovndb_etcd_tcpdump} == "true" ]]; then
 	echo "================= start logging with tcpdump ============================ "
 	tcpdump -nnv -i any  port '(6641 or 6642)' -s 65535  -w /var/log/openvswitch/tcpdump.pcap -C 1000 -Z root > /var/log/openvswitch/tcpdump_logs.log 2>&1 &
   fi
